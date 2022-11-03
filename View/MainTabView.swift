50955eae086a3087a880b57ac24d22b437f28527
ShapeKim98 <talkers.potters-0p@icloud.com>
2022-11-03 19:39:06 +0900

    add MainTabView

---
+++ View/MainTabView.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabView.swift
+//  PrototypeIF2.0
+//
+//  Created by 김도형 on 2022/09/22.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Group {
+                MainView()
+                    .tabItem {
+                        Label("메인", systemImage: "house")
+                    }
+                
+//                CommunityView()
+//                    .tabItem {
+//                        Label("커뮤티니", systemImage: "person.2")
+//                    }
+                
+//                NotificationHistoryView()
+//                    .tabItem {
+//                        Label("알림내역", systemImage: "bell")
+//                    }
+                
+//                MenuView()
+//                    .tabItem {
+//                        if #available(iOS 15.0, *) {
+//                            Label("메뉴", systemImage: "line.3.horizontal")
+//                        } else {
+//                            Label("메뉴", systemImage: "list.bullet")
+//                        }
+//                    }
+            }
+            .modifier(VersionedTabBarColorModifier(color: Color("bkg")))
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
